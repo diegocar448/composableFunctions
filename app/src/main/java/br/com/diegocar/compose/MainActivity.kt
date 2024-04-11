b6f2909d1b62e1b6736241f0fd6691edc2422965
Diego Cardoso <diegocar448@hotmail.com>
2024-04-11 10:31:03 -0300

    ComposableFunctions

--- app/src/main/java/br/com/diegocar/compose/MainActivity.kt
+++ app/src/main/java/br/com/diegocar/compose/MainActivity.kt
@@ -1,14 +1,18 @@
     package br.com.diegocar.compose
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.diegocar.compose.ui.theme.ComposeTheme
 
@@ -23,11 +27,34 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    MainScreenPreview()
                 }
             }
         }
     }
 }
+
+    @Composable
+    fun MainScreen() {
+        // Text(text="Texto com Compose")
+        // Text(text="qualquer coisa")
+
+        Button(
+            onClick = {},
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_bus),
+                contentDescription = null // adicione uma descrição, se necessário
+            )
+            Text(text = "Confirmar")
+        }
+
+    }
+    @Preview
+    @Composable
+    fun MainScreenPreview(){
+        MainScreen()
+    }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
