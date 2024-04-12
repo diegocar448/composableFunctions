09389525d5d6929cb6024f85fbb9f4161235302d
Diego Cardoso <diegocar448@hotmail.com>
2024-04-12 11:23:02 -0300

    GerenciandoEstados

--- app/src/main/java/br/com/diegocar/compose/MainActivity.kt
+++ app/src/main/java/br/com/diegocar/compose/MainActivity.kt
@@ -2,24 +2,34 @@
 
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -47,7 +57,9 @@ import br.com.diegocar.compose.ui.theme.ComposeTheme
                     color = MaterialTheme.colorScheme.background
                 ) {
                     // Greeting("Android")
-                    MainScreenPreview()
+                    // MainScreenPreview()
+                    MainScreen()
+
                 }
             }
         }
@@ -55,18 +67,60 @@ import br.com.diegocar.compose.ui.theme.ComposeTheme
 }
 
     @Composable
+    fun ClickCounter(clicks: Int, onClick: () -> Unit){
+        Column {
+            Button(onClick = onClick){
+                Text("Testando");
+            }
+        }
+    }
+
+    @Composable
     fun MainScreen() {
+        var clicks by remember {mutableStateOf(0)}
+
+        //RowsColumns()
         Column(
-        ) {
-            DogCard(
-                dog = Dog("Luna",  "Chow Chow"),
-                modifier = Modifier.background(Color.Green)
-            )
-            DogCard(
-                dog = Dog("Orbs",  "Caramelo"),
-                modifier = Modifier.background(Color.Blue)
+            modifier = Modifier.fillMaxWidth()
+        ){
+            ClickCounter(clicks = clicks){
+                clicks++
+            }
+
+            HelloContent()
+        }
+    }
+
+
+    @Composable //Estados
+    fun HelloContent() {
+        Column(modifier = Modifier.padding(16.dp)) {
+            var name by remember { mutableStateOf("") }
+            if (name.isNotEmpty()) {
+                Text(
+                    text = "Hello, $name!",
+                    modifier = Modifier.padding(bottom = 8.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Name") }
             )
         }
+    }
+
+    @Composable
+    private fun RowsColumns() {
+        DogCard(
+            dog = Dog("Luna", "Chow Chow"),
+            modifier = Modifier.background(Color.Green)
+        )
+        DogCard(
+            dog = Dog("Orbs", "Caramelo"),
+            modifier = Modifier.background(Color.Blue)
+        )
     }
 
     data class Dog(
